8581d8e7795d6386ef6be1cd0b271f96f8288863
mercy <mihretyirga7@gmail.com>
2025-10-15 23:17:14 +0300

    fixing route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -6,12 +7,11 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <StrictMode>
     <AppProvider>
-      
+      <BrowserRouter>
     <App />
-  
+  </BrowserRouter>
     </AppProvider>
-    </BrowserRouter>
- 
+  </StrictMode>,
 )
