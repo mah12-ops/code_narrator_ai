76b509b6efce017c65f0398279906b3b41f58e1a
mercy <mihretyirga7@gmail.com>
2025-10-16 13:45:54 +0300

    fix you tube link

--- client/src/components/pages/LandingPage/Demo.tsx
+++ client/src/components/pages/LandingPage/Demo.tsx
@@ -19,14 +19,15 @@ function Demo() {
             <motion.div initial={{ opacity: 0, x: 20 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true }} transition={{ duration: 0.6 }} className="order-1 md:order-2">
               <div className="relative rounded-2xl border border-white/10 bg-[#0a0a0a] p-3 shadow-2xl">
                 <div className="aspect-video w-full rounded-xl overflow-hidden">
-    <iframe
+   <iframe
   className="h-full w-full"
-  src="https://youtu.be/wj6LB7uS7A4?list=PL6qroReUMetpY0NkraVXqDXelBxMVvlcn"
+  src="https://www.youtube.com/embed/wj6LB7uS7A4?si=0QDux6s1mxh7269"
   title="CodeNarrator Demo"
   frameBorder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
   allowFullScreen
 ></iframe>
+
 
 
                 </div>
