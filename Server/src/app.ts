09b1feda7d819a24cc684bd1acfd2528a832dbcd
mercy <mihretyirga7@gmail.com>
2025-11-26 10:27:32 +0300

    updated

---
+++ Server/src/app.ts
@@ -0,0 +1,49 @@
+// src/app.ts
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { rootRouter } from "./route/root";
+import path from "path";
+
+dotenv.config();
+
+export const createApp = () => {
+  const app = express();
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://code-narrator-ai.vercel.app"
+    ];
+
+    if (origin && allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+
+    next();
+  });
+
+  app.use(express.json());
+  app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+  app.use("/api", rootRouter);
+
+  return app;
+};
