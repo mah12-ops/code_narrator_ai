bc4559b693a3f7de31c5d180b11b4345adcc5453
mercy <mihretyirga7@gmail.com>
2025-11-26 22:51:00 +0300

    updated

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,7 +5,10 @@ import dotenv from "dotenv";
 import { rootRouter } from "./route/root";
 import path from "path";
 
-dotenv.config();
+const envFile =
+  process.env.NODE_ENV === "test" ? ".env.test" : ".env";
+
+dotenv.config({ path: envFile });
 
 export const createApp = () => {
   const app = express();
