7c94fe6c6b31834c0c60e07d303975239537cb64
mercy <mihretyirga7@gmail.com>
2025-11-27 09:15:04 +0300

    updated

--- Server/src/prisma/prisma.ts
+++ Server/src/prisma/prisma.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
 
+
+export const prisma = new PrismaClient();
 if (process.env.NODE_ENV === "test") {
   config({ path: ".env.test" });
 } else {
